73d4ac406adf01118c54788b36b17355540426e2
Cypress <cypress@lumafield.com>
2023-10-18 14:27:54 -0400

    Update all subscribers if a sub-path is modified

--- src/state-context.tsx
+++ src/state-context.tsx
@@ -12,11 +12,7 @@ export const StateContext = createContext<{
   applyPatch: (patch: Operation[]) => void;
 } | null>(null);
 
-export function StateProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}): JSX.Element {
+export function StateProvider({ children }: { children: React.ReactNode }) {
   const stateRef = useRef<State>({});
   const subscribersRef = useRef<Map<string, Set<Subscriber>>>(new Map());
   const applyPatch = useCallback((patch: Operation[]) => {
@@ -27,19 +23,26 @@ export function StateProvider({
         case "remove":
         case "replace":
         case "copy": {
-          const subscribers = subscribersRef.current.get(operation.path);
+          const subscribers = getSubscribers(
+            operation.path,
+            subscribersRef.current
+          );
           if (subscribers) {
             subscribers.forEach((subscriber) => subscriber());
           }
           break;
         }
         case "move": {
-          const subscribersForPath = subscribersRef.current.get(operation.from);
+          const subscribersForPath = getSubscribers(
+            operation.from,
+            subscribersRef.current
+          );
           if (subscribersForPath) {
             subscribersForPath.forEach((subscriber) => subscriber());
           }
-          const subscribersForToPath = subscribersRef.current.get(
-            operation.path
+          const subscribersForToPath = getSubscribers(
+            operation.path,
+            subscribersRef.current
           );
           if (subscribersForToPath) {
             subscribersForToPath.forEach((subscriber) => subscriber());
@@ -73,4 +76,35 @@ export function StateProvider({
       {children}
     </StateContext.Provider>
   );
+}
+
+/**
+ * If a path is a subset of another path, then the subscribers for the parent path should be notified
+ *
+ * @example
+ *
+ * const subscribersByPath = new Map([
+ *  ["/", new Set([callback1])],
+ *  ["/a", new Set([callback2])],
+ *  ["/a/b/c", new Set([callback3])],
+ *  ["/a/b/x", new Set([callback4])],
+ *  ["/x", new Set([callback5])],
+ * ]);
+ *
+ * getSubscribers("/a/b/c", subscribersByPath) // [callback1, callback2, callback3]
+ *
+ * @param path the path for the state change
+ * @param subscribersByPath a map of subscribers by the path they're subscribed to
+ */
+function getSubscribers(
+  path: string,
+  subscribersByPath: Map<string, Set<Subscriber>>
+): Set<Subscriber> {
+  const result = new Set<Subscriber>();
+  for (const [key, subscribers] of subscribersByPath) {
+    if (path.startsWith(key)) {
+      subscribers.forEach((subscriber) => result.add(subscriber));
+    }
+  }
+  return result;
 }
